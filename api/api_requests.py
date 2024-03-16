5faddd29dfa0ccfbf3de5e8ddec5e06a49f81c68
Marat <marat.galimov@gcore.com>
2024-03-16 12:52:46 +0500

    improve code

--- api/api_requests.py
+++ api/api_requests.py
@@ -3,43 +3,36 @@ from api import api_endpoints
 
 class BaseRequests:
     def __init__(self, client, bearer_token):
-        self.client = client
-        self.bearer_token = bearer_token
+        self.__client = client
+        self.__bearer_token = bearer_token
+        self.__headers = {'Authorization': f"Bearer {self.__bearer_token}"}
+        self.endpoint_images = api_endpoints.ImagesEndpoints.IMAGES
 
     def get_obj(self, endpoint, params):
-        headers = {'Authorization': f"Bearer {self.bearer_token}"}
-        return self.client.get(endpoint, headers=headers, params=params)
+        return self.__client.get(endpoint, headers=self.__headers, params=params)
 
     def post_obj(self, endpoint, **kwargs):
-        headers = {'Authorization': f"Bearer {self.bearer_token}"}
-        return self.client.post(endpoint, headers=headers, **kwargs)
+        return self.__client.post(endpoint, headers=self.__headers, **kwargs)
 
     def patch_obj(self, endpoint, **kwargs):
-        headers = {'Authorization': f"Bearer {self.bearer_token}"}
-        return self.client.patch(endpoint, headers=headers, **kwargs)
+        return self.__client.patch(endpoint, headers=self.__headers, **kwargs)
 
     def delete_obj(self, endpoint):
-        headers = {'Authorization': f"Bearer {self.bearer_token}"}
-        return self.client.delete(endpoint, headers=headers)
+        return self.__client.delete(endpoint, headers=self.__headers)
 
 
 class ImagesRequests(BaseRequests):
     def get_images(self, params=None):
-        endpoint = api_endpoints.ImagesEndpoints.IMAGES
-        return self.get_obj(endpoint, params)
+        return self.get_obj(self.endpoint_images, params)
 
     def get_image_by_id(self, obj_id, params=None):
-        endpoint = f"{api_endpoints.ImagesEndpoints.IMAGES}/{obj_id}"
-        return self.get_obj(endpoint, params)
+        return self.get_obj(f"{self.endpoint_images}/{obj_id}", params)
 
     def create_image(self, request_body):
-        endpoint = api_endpoints.ImagesEndpoints.IMAGES
-        return self.post_obj(endpoint, json=request_body)
+        return self.post_obj(self.endpoint_images, json=request_body)
 
     def update_image(self, obj_id, request_body):
-        endpoint = f"{api_endpoints.ImagesEndpoints.IMAGES}/{obj_id}"
-        return self.patch_obj(endpoint, json=request_body)
+        return self.patch_obj(f"{self.endpoint_images}/{obj_id}", json=request_body)
 
     def delete_image(self, obj_id):
-        endpoint = f"{api_endpoints.ImagesEndpoints.IMAGES}/{obj_id}"
-        return self.delete_obj(endpoint)
+        return self.delete_obj(f"{self.endpoint_images}/{obj_id}")
