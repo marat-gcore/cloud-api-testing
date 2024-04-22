ace8495ea1f8ed9759f6382aeadf50821ba9ee13
Marat <marat.galimov@gcore.com>
2024-04-22 10:35:25 +0500

    fix CI

--- api/api_endpoints.py
+++ api/api_endpoints.py
@@ -4,7 +4,8 @@ from enum import Enum
 
 class Endpoints(str, Enum):
     TASKS = f"https://{os.getenv('PREPROD_URL')}/v1/tasks"
-    IMAGES = f"https://{os.getenv('PREPROD_URL')}/v1/images/{os.getenv('PROJECT_ID')}/{os.getenv('REGION_ID')}"
+    IMAGES = f"https://cloud-api-preprod.cloud.gc.onl/v1/images/{os.getenv('PROJECT_ID')}/{os.getenv('REGION_ID')}"
+    # IMAGES = f"https://{os.getenv('PREPROD_URL')}/v1/images/{os.getenv('PROJECT_ID')}/{os.getenv('REGION_ID')}"
 
     def __str__(self) -> str:
         return self.value
